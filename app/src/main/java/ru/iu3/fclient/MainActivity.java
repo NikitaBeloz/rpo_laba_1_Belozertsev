e067e3fcd4c8342a3bbdf0056ae87d0bf5f8dc6f
Nikita Belozertsev <Belozertsev@yandex.ru>
2025-03-12 22:14:48 +0300

    final laboratory work 3

--- app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -18,9 +18,10 @@ import org.apache.commons.codec.binary.Hex;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TransactionEvents {
 
     ActivityResultLauncher activityResultLauncher;
+    private String pin;
 
     // Used to load the 'fclient' library on application startup.
     static {
@@ -36,29 +37,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         int res = initRng();
-        byte[] v = randomBytes(10);
 
-        byte[] key = randomBytes(16);
-        byte[] encryptedData = encrypt(key, v);
-        byte[] decryptData = decrypt(key, encryptedData);
-
-//        TextView tv = findViewById(R.id.sample_text);
-//        tv.setText(stringFromJNI());
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//        // Example of a call to a native method
-//        TextView tv = binding.sampleText;
-//        tv.setText(stringFromJNI());
 
         activityResultLauncher  = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
                     if (result.getResultCode() == Activity.RESULT_OK) {
                         Intent data = result.getData();
-                        String pin = data.getStringExtra("pin");
-                        Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        // обработка результата
+                        //String pin = data.getStringExtra("pin");
+                        //Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        pin = data.getStringExtra("pin");
+                        synchronized (MainActivity.this) {
+                            MainActivity.this.notifyAll();
+                        }
                     }
                 });
     }
@@ -68,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native boolean transaction(byte[] trd);
     public static native int initRng();
     public static native byte[] randomBytes(int no);
 
@@ -91,8 +85,38 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonClick(View v)
     {
-        Intent it = new Intent(this, PinpadActivity.class);
-        //startActivity(it);
-        activityResultLauncher.launch(it);
+        new Thread(()-> {
+            try {
+                byte[] trd = stringToHex("9F0206000000000100");
+                transaction(trd);
+            } catch (Exception ex) {
+                // todo: log error
+            }
+        }).start();
+    }
+
+
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+        Intent it = new Intent(MainActivity.this, PinpadActivity.class);
+        it.putExtra("ptc", ptc);
+        it.putExtra("amount", amount);
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(it);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception ex) {
+                //todo: log error
+            }
+        }
+        return pin;
+    }
+
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(()-> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+        });
     }
 }
